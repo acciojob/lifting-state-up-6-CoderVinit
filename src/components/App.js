201bb09fbc544d6851941973e14621b067cf8c61
VINIT KUMAR PATEL <101181057+CoderVinit@users.noreply.github.com>
2025-02-22 19:16:18 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,8 @@ import './../styles/App.css';
 const App = () => {
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", completed: false },
-    { id: 2, text: "Practice DSA", completed: false },
-    { id: 3, text: "Build a Project", completed: false },
+    { id: 2, text: "Build a React app", completed: false },
+    { id: 3, text: "Deply the React app", completed: false },
   ]);
 
   const handleComplete = (id) => {
